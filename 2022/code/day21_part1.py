6e245a7b0bbbe838822c772008677a84c23bd692
Daniel Soukup <dsoukup@brainstation.io>
2022-12-21 16:35:28 -0500

    d21p1 sols

---
+++ 2022/code/day21_part1.py
@@ -0,0 +1,82 @@
+import re
+import networkx as nx
+
+INPUT_FILE = "2022/inputs/day21.txt"
+
+
+def node_from_string(line: str) -> dict[str]:
+    node_name, definition = line.split(":")
+
+    node = dict()
+    node["name"] = node_name
+
+    numbers = re.findall(r"\d+", definition)
+    if numbers:
+        node["value"] = int(numbers[0])
+        node["operation"] = None
+        node["neighbors"] = []
+    else:
+        left, operation, right = definition.strip().split(" ")
+        node["operation"] = operation
+        node["neighbors"] = [left, right]
+        node["value"] = None
+
+    return node
+
+
+def digraph_from_nodes(nodes: list) -> nx.DiGraph:
+    digraph = nx.DiGraph()
+    for node in nodes:
+        digraph.add_node(node["name"], value=node["value"], operation=node["operation"])
+        for n in node["neighbors"]:
+            digraph.add_edge(n, node["name"])
+
+    # nx.set_node_attributes(digraph, {node["name"]: node["value"] for node in nodes},"value")
+
+    return digraph
+
+
+def find_node_value(digraph: nx.DiGraph, node: str) -> int:
+    face_value = digraph.nodes[node]["value"]
+
+    if face_value is None:
+        # recursive call
+        children = list(digraph.predecessors(node))
+        child_values = [find_node_value(digraph, child) for child in children]
+
+        assert (
+            len(child_values) == 2
+        ), f"There should be two values instead {len(child_values)}"
+
+        # eval operation symbol
+        operation = digraph.nodes[node]["operation"]
+        if operation == "+":
+            value = child_values[0] + child_values[1]
+        elif operation == "-":
+            value = child_values[0] - child_values[1]
+        elif operation == "*":
+            value = child_values[0] * child_values[1]
+        elif operation == "/":
+            value = child_values[0] / child_values[1]
+        else:
+            raise UserWarning(f"Something is wrong, unknown operation {operation}...")
+
+        digraph.nodes[node]["value"] = value
+        return value
+    else:
+        return face_value
+
+
+if __name__ == "__main__":
+    nodes = []
+    with open(INPUT_FILE, "r") as file:
+        for line in file:
+            line = line.strip()
+            node = node_from_string(line)
+            nodes.append(node)
+
+    digraph = digraph_from_nodes(nodes)
+
+    root_value = find_node_value(digraph, "root")
+
+    print(root_value)
--- 2022/code/day21_part2.py
+++ 2022/code/day21_part2.py
@@ -1,10 +1,10 @@
+INPUT_FILE = "2022/inputs/day21.txt"
 
 
 if __name__ == "__main__":
     total = 0
     with open(INPUT_FILE, "r") as file:
         for line in file:
-            line = line.strip()
             pass
 
     print(total)
