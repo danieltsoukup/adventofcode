66b5028510c13004a70d7133aab1e88887ffa4e1
Daniel Soukup <dsoukup123@gmail.com>
2023-12-11 17:16:18 -0500

    solve day8

---
+++ 2023/code/day9_part2.py
@@ -0,0 +1,11 @@
+INPUT_FILE = "2023/inputs/day9.txt"
+
+
+if __name__ == "__main__":
+    total = 0
+    with open(INPUT_FILE, "r") as file:
+        for line in file:
+            line = line.strip()
+            pass
+
+    print(total)
